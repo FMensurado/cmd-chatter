30ddefcd0eef6aca8f7787fb2a8c661cc69edc26
Frederico Mensurado <fmensurado@intercement.com>
2017-11-30 17:32:08 +0000

    testing initial commit on openshift

--- index.js
+++ index.js
@@ -72,5 +72,5 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 http.listen(port, ipaddress, function(){
-  console.log('listening on *:3000');
+  console.log('listening on ' + ipaddress + ':' + port);
 });
